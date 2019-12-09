51894142fb471bc0057eba6c18813512169bb237
m06800825 <40761610+m06800825@users.noreply.github.com>
2019-12-09 22:41:42 +0800

    [A] add menu to pick player

---
+++ play.py
@@ -0,0 +1,197 @@
+import os
+import pygame as pg
+import time
+import math
+from utils import Player, Flower, Arrow
+
+
+def main():
+    pg.init()
+    
+    # music and sound
+    sound_path = os.path.join("music", "sound.wav")
+    music_path = os.path.join("music", "pacific.mp3")
+    play_bg_music(music_path)
+
+    # initialize game window
+    canvas_width = 1200
+    canvas_height = 750
+    canvas = pg.display.set_mode((canvas_width, canvas_height))
+    pg.display.set_caption("Catch Flower Game")
+
+    # initialize group
+    arrows = pg.sprite.Group()
+    flowers = pg.sprite.Group()
+    players = pg.sprite.Group()
+
+    # initialize menu
+    date = pg.image.load(os.path.join('image', 'date.png'))
+    date = pg.transform.scale(date, (1200, 750))
+    arrow = Arrow()
+    arrows.add(arrow)
+
+    # initialize background
+    grassland = pg.image.load(os.path.join('image', 'grassland.png'))
+    grassland = pg.transform.scale(grassland, (1200, 750))
+
+    # initialize time
+    clock = pg.time.Clock()
+    start_time = time.time()
+    sec = 0
+
+    # initialize score
+    score = 0
+
+    # initialize game message
+    score_font = pg.font.SysFont("Arial", 40)
+    time_font = pg.font.SysFont("Arial", 40)
+    message_font = pg.font.SysFont("Arial", 60)
+    menu_msg = "[PRESS SPACE TO CHOOSE PLAYER]"
+    end_msg = ""
+    start_msg = "[PRESS SPACE TO START]"
+
+    # initialize game status
+    running = True
+    menu = True
+    playing = False
+
+    while running:
+        clock.tick(30)
+
+        if menu:
+            for event in pg.event.get():
+                # leave game
+                if event.type == pg.QUIT:
+                    running = False
+                # check button down
+                if event.type == pg.KEYDOWN:
+                    # check ESC down
+                    if event.key == pg.K_ESCAPE:
+                        running = False
+                    # change arrow location
+                    if event.key == pg.K_LEFT:
+                        arrow.update(380)
+                    elif event.key == pg.K_RIGHT:
+                        arrow.update(720)     
+                    # choose player                 
+                    if event.key == pg.K_SPACE and arrow.rect.x == 380:
+                        player = Player("ken")
+                        players.add(player)
+                        menu = False
+                    elif event.key == pg.K_SPACE and arrow.rect.x == 720:
+                        player = Player("jane")
+                        players.add(player)
+                        menu = False
+
+            canvas.blit(date, (0,0))
+            menu_logo = message_font.render(menu_msg, True, (255, 0, 0))
+            canvas.blit(menu_logo, (160, 650))
+            arrows.draw(canvas)
+
+        else:
+            for event in pg.event.get():
+                # leave game
+                if event.type == pg.QUIT:
+                    running = False
+                # check button down
+                if event.type == pg.KEYDOWN:
+                    # check ESC down
+                    if event.key == pg.K_ESCAPE:
+                        running = False
+                    # check left and right
+                    if event.key == pg.K_LEFT:
+                        player.speed = -20
+                    elif event.key == pg.K_RIGHT:
+                        player.speed = 20
+                    elif event.key == pg.K_DOWN:
+                        player.speed = 0
+                    # start playing 
+                    if event.key == pg.K_SPACE:
+                        if not playing:
+                            playing = True
+                            flowers.empty()
+                            # initailize
+                            score = 0
+                            player.speed = 0
+                            player.rect.x = 525
+                            start_time = time.time()
+                            gen_flower_time = time.time()
+            
+            if playing:
+                sec = count_second(start_time)
+                if sec == 30:
+                    end_msg = "[TIME'S UP]"
+                    playing = False
+
+                time_difference = int((time.time() - gen_flower_time) * 1000)
+                # Flower
+                if time_difference > 500:
+                    gen_flower_time += 0.5 # generate a flower per 0.5 second
+                    f = Flower()
+                    flowers.add(f)
+                
+                # detect collide
+                hit_flower = pg.sprite.spritecollide(player, flowers, True)  
+                if len(hit_flower) > 0:
+                    play_sound(sound_path)
+                    score += len(hit_flower)
+
+                # update objects
+                player.update()
+                for flower in flowers:
+                    flower.update()
+
+            # update score
+            score_msg = "Score: " + str(score)
+            score_msg = score_font.render(score_msg, True, (255, 0, 0))
+
+            # update time
+            time_msg = "Time: " + str(30-sec)
+            time_msg = time_font.render(time_msg, True, (255, 100, 0))
+
+            # draw 
+            canvas.blit(grassland, (0,0))
+            canvas.blit(score_msg, (10, 10))
+            canvas.blit(time_msg, (1050, 10))
+            flowers.draw(canvas)
+            players.draw(canvas)
+
+            if not playing:
+                # show message
+                end_logo = message_font.render(end_msg, True, (0, 0, 255))
+                start_logo = message_font.render(start_msg, True, (0, 0, 255))
+                canvas.blit(end_logo, (480, 330))
+                canvas.blit(start_logo, (300, 400))
+
+        pg.display.update()
+
+    pg.quit()
+    quit()
+
+
+def play_bg_music(music_path):
+    # load music
+    pg.mixer.music.load(music_path)
+    # set volume
+    pg.mixer.music.set_volume(0.5)
+    # recursive playing
+    pg.mixer.music.play(-1, 0)
+
+
+def play_sound(sound_path):
+    # load sound
+    sound = pg.mixer.Sound(sound_path)
+    # set volume
+    sound.set_volume(0.3)
+    # play
+    sound.play()
+
+
+def count_second(start_time):
+    last_time = time.time()
+    total_time = math.floor(last_time - start_time)
+    return total_time
+
+
+if __name__ == "__main__":
+    main()
---
+++ utils.py
@@ -0,0 +1,51 @@
+import pygame as pg
+import os
+import random
+
+class Player(pg.sprite.Sprite):
+    def __init__(self, name="ken"):
+        pg.sprite.Sprite.__init__(self)
+        self.name = name
+        if self.name == "jane":
+            self.image = pg.image.load(os.path.join('image', 'jane_removebg.png')).convert_alpha()
+        else:
+            self.image = pg.image.load(os.path.join('image', 'ken_removebg.png')).convert_alpha()
+        self.image = pg.transform.scale(self.image, (125, 200))
+        self.rect = self.image.get_rect()
+        self.rect.x = 525
+        self.rect.y = 550
+        self.speed = 0
+
+    def update(self):
+        self.rect.x += self.speed
+        # check not to exceed canvas
+        if self.rect.x > (1200 - self.rect.width):
+            self.rect.x = 1200 - self.rect.width
+        elif self.rect.x < 0:
+            self.rect.x = 0
+
+
+class Flower(pg.sprite.Sprite):
+    def __init__(self):
+        pg.sprite.Sprite.__init__(self)
+        self.image = pg.image.load(os.path.join('image', 'flower_removebg.png')).convert_alpha()
+        self.image = pg.transform.scale(self.image, (50, 50))
+        self.rect = self.image.get_rect()
+        self.rect.x = int(random.random() * 1150)
+        self.rect.y = 0
+
+    def update(self):
+        self.rect.y += 20
+
+
+class Arrow(pg.sprite.Sprite):
+    def __init__(self):
+        pg.sprite.Sprite.__init__(self)
+        self.image = pg.image.load(os.path.join('image', 'arrow_removebg.png')).convert_alpha()
+        self.image = pg.transform.scale(self.image, (80, 120))
+        self.rect = self.image.get_rect()
+        self.rect.x = 380
+        self.rect.y = 20
+    
+    def update(self, x_loc):
+        self.rect.x = x_loc
